9d54ece213e19fc9efe3b1947b6251ba4626d989
Ledragon <hugues.stefanski@gmail.com>
2015-02-23 19:41:27 +0100

    Build process updated span replaced by i for icon use of template replacer

--- dist/angular-formly-templates-bootstrap.js
+++ dist/angular-formly-templates-bootstrap.js
@@ -16,87 +16,96 @@
 }(this, function (angular) {
 
 angular.module('formlyBootstrap', ['formly'], ["formlyConfigProvider", function configFormlyVanilla(formlyConfigProvider) {
-  'use strict';
+    'use strict';
 
-  formlyConfigProvider.setWrapper([
-    {
-      name: 'bootstrapLabel',
-      templateUrl: 'wrappers/formly-wrappers-bootstrap-label.html'
-    },
-    {
-      name: 'bootstrapHasError',
-      templateUrl: 'wrappers/formly-wrappers-bootstrap-has-error.html'
-    },
-    {
-      name: 'bootstrapAddons',
-      templateUrl: 'wrappers/formly-wrappers-bootstrap-addons.html'
-    }
-  ]);
+    formlyConfigProvider.setWrapper([{
+            name: 'bootstrapLabel',
+            templateUrl: 'wrappers/formly-wrappers-bootstrap-label.html'
+        },
+        {
+            name: 'bootstrapHasError',
+            templateUrl: 'wrappers/formly-wrappers-bootstrap-has-error.html'
+        }
+    ]);
 
-  var commonWrappers = ['bootstrapAddons','bootstrapLabel', 'bootstrapHasError'];
+    var commonWrappers = ['bootstrapLabel', 'bootstrapHasError'];
 
-  angular.forEach(['radio', 'select'], function(fieldName) {
-    formlyConfigProvider.setType({
-      name: fieldName,
-      templateUrl: getFieldTemplateUrl(fieldName),
-      wrapper: commonWrappers
+    angular.forEach(['radio', 'select'], function (fieldName) {
+        formlyConfigProvider.setType({
+            name: fieldName,
+            templateUrl: getFieldTemplateUrl(fieldName),
+            wrapper: commonWrappers
+        });
     });
-  });
-  formlyConfigProvider.setType({
-    name: 'input',
-    template: '<input class="form-control" ng-model="model[options.key]">',
-    wrapper: commonWrappers
-  });
+    formlyConfigProvider.setType({
+        name: 'input',
+        template: '<input class="form-control" ng-model="model[options.key]">',
+        wrapper: commonWrappers
+    });
 
-  // textarea has custom defaultOptions
-  formlyConfigProvider.setType({
-    name: 'textarea',
-    template: '<textarea class="form-control" ng-model="model[options.key]"></textarea>',
-    wrapper: commonWrappers,
-    defaultOptions: {
-      data: {
-        ngModelAttributes: {rows: 'rows', cols: 'cols'}
-      }
+    // textarea has custom defaultOptions
+    formlyConfigProvider.setType({
+        name: 'textarea',
+        template: '<textarea class="form-control" ng-model="model[options.key]"></textarea>',
+        wrapper: commonWrappers,
+        defaultOptions: {
+            data: {
+                ngModelAttributes: {
+                    rows: 'rows',
+                    cols: 'cols'
+                }
+            }
+        }
+    });
+
+    // checkbox doesn't have a bootstrapLabel wrapper
+    formlyConfigProvider.setType({
+        name: 'checkbox',
+        templateUrl: getFieldTemplateUrl('checkbox'),
+        wrapper: ['bootstrapHasError']
+    });
+
+    formlyConfigProvider.templateManipulators.preWrapper.push(function ariaDescribedBy(template, options, scope) {
+        if (options.templateOptions && angular.isDefined(options.templateOptions.description) &&
+            options.type !== 'radio' && options.type !== 'checkbox') {
+            var el = angular.element('<a></a>');
+            el.append(template);
+            var modelEls = angular.element(el[0].querySelectorAll('[ng-model]'));
+            if (modelEls) {
+                el.append(
+                    '<p id="' + scope.id + '_description"' +
+                    'class="help-block"' +
+                    'ng-if="options.templateOptions.description">' +
+                    '{{options.templateOptions.description}}' +
+                    '</p>'
+                );
+                modelEls.attr('aria-describedby', scope.id + '_description');
+                return el.html();
+            } else {
+                return template;
+            }
+        } else {
+            return template;
+        }
+    });
+
+
+    formlyConfig.templateManipulators.postWrapper.push(function (template, options) {
+        if (!options.templateOptions.addonLeft && !options.templateOptions.addonRight) {
+            return template;
+        }
+        return $http.get('other/formly-templates-bootstrap-addons.html', {
+            cache: $templateCache
+        }).then(function (response) {
+            return response.data.replace('<formly-transclude></formly-transclude>', template);
+        });
+    });
+
+    function getFieldTemplateUrl(name) {
+        return 'fields/formly-field-' + name + '.html';
     }
-  });
-
-  // checkbox doesn't have a bootstrapLabel wrapper
-  formlyConfigProvider.setType({
-    name: 'checkbox',
-    templateUrl: getFieldTemplateUrl('checkbox'),
-    wrapper: ['bootstrapHasError']
-  });
-
-  formlyConfigProvider.templateManipulators.preWrapper.push(function ariaDescribedBy(template, options, scope) {
-    if (options.templateOptions && angular.isDefined(options.templateOptions.description) &&
-      options.type !== 'radio' && options.type !== 'checkbox') {
-      var el = angular.element('<a></a>');
-      el.append(template);
-      var modelEls = angular.element(el[0].querySelectorAll('[ng-model]'));
-      if (modelEls) {
-        el.append(
-          '<p id="' + scope.id + '_description"' +
-              'class="help-block"' +
-              'ng-if="options.templateOptions.description">' +
-            '{{options.templateOptions.description}}' +
-          '</p>'
-        );
-        modelEls.attr('aria-describedby', scope.id + '_description');
-        return el.html();
-      } else {
-        return template;
-      }
-    } else {
-      return template;
-    }
-  });
-
-  function getFieldTemplateUrl(name) {
-    return 'fields/formly-field-' + name + '.html';
-  }
 
 }]);
-
 angular.module('formlyBootstrap').run(['$templateCache', function($templateCache) {
   'use strict';
 
@@ -115,11 +124,6 @@ angular.module('formlyBootstrap').run(['$templateCache', function($templateCache
   );
 
 
-  $templateCache.put('wrappers/formly-wrappers-bootstrap-addons.html',
-    "<div ng-class=\"{'input-group': to.addonLeft || to.addonRight}\"><div class=input-group-addon ng-if=to.addonLeft><span ng-class=\"'{{to.addonLeft.class}}'\" ng-if=to.addonLeft.class></span> <span ng-if=to.addonLeft.text>{{to.addonLeft.text}}</span></div><formly-transclude></formly-transclude><div class=input-group-addon ng-if=to.addonRight><span ng-class=\"'{{to.addonRight.class}}'\" ng-if=to.addonRight.class></span> <span ng-if=to.addonRight.text>{{to.addonRight.text}}</span></div></div>"
-  );
-
-
   $templateCache.put('wrappers/formly-wrappers-bootstrap-has-error.html',
     "<div class=form-group ng-class=\"{'has-error': options.formControl.$invalid && options.formControl.$touched}\"><formly-transclude></formly-transclude></div>"
   );
@@ -127,6 +131,11 @@ angular.module('formlyBootstrap').run(['$templateCache', function($templateCache
 
   $templateCache.put('wrappers/formly-wrappers-bootstrap-label.html',
     "<div><label for={{id}} class=control-label>{{options.templateOptions.label}} {{options.templateOptions.required ? '*' : ''}}</label><formly-transclude></formly-transclude></div>"
+  );
+
+
+  $templateCache.put('other/formly-wrappers-bootstrap-addons.html',
+    "<div ng-class=\"{'input-group': to.addonLeft || to.addonRight}\"><div class=input-group-addon ng-if=to.addonLeft><i class=\"'{{to.addonLeft.class}}'\" ng-if=to.addonLeft.class></i> <span ng-if=to.addonLeft.text>{{to.addonLeft.text}}</span></div><formly-transclude></formly-transclude><div class=input-group-addon ng-if=to.addonRight><i class=\"'{{to.addonRight.class}}'\" ng-if=to.addonRight.class></i> <span ng-if=to.addonRight.text>{{to.addonRight.text}}</span></div></div>"
   );
 
 }]);
