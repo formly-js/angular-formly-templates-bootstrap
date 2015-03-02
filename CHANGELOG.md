6ef5282fd97c3db7d81f80930b497e31b3d6249a
Kent C. Dodds <kent+github@doddsfamily.us>
2015-03-02 16:07:48 -0700

    Fixing issue with dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 4.0.1
+
+- Fixing external dependencies
+
 ## 4.0.0
 
 - Added addonLeft and addonRight to place add-ons on fields
--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,18 @@ var baseConfig = {
 
   externals: {
     angular: 'angular',
-    'angular-formly': 'ngFormly',
-    'api-check': 'apiCheck'
+    'angular-formly': {
+      root: 'ngFormly',
+      amd: 'angular-formly',
+      commonjs2: 'angular-formly',
+      commonjs: 'angular-formly'
+    },
+    'api-check': {
+      root: 'apiCheck',
+      amd: 'api-check',
+      commonjs2: 'api-check',
+      commonjs: 'api-check'
+    }
   },
 
   plugins: [],
