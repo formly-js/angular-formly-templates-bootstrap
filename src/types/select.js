bbeba7e2a17749a0f4088ca80b639e34997fc516
Kent C. Dodds <kent+github@doddsfamily.us>
2015-05-17 00:23:34 -0600

    Fixes bug in last release where you couldn't actually override anything.

--- src/types/select.js
+++ src/types/select.js
@@ -1,22 +1,24 @@
 export default  ngModule => {
   ngModule.config(addSelectType);
 
+  const template = `<select class="form-control" ng-model="model[options.key]"></select>`;
+
   function addSelectType(formlyConfigProvider, formlyBootstrapApiCheck) {
     const c = formlyBootstrapApiCheck;
     formlyConfigProvider.setType({
       name: 'select',
-      template: require('./select.html'),
+      template,
       wrapper: ['bootstrapLabel', 'bootstrapHasError'],
       defaultOptions(options) {
-        if (options.templateOptions.ngOptions) {
-          return {
-            ngModelAttrs: {
-              [options.templateOptions.ngOptions]: {
-                value: 'ng-options'
-              }
+        /* jshint maxlen:195 */
+        let ngOptions = options.templateOptions.ngOptions || `option[to.valueProp || 'value'] as option[to.labelProp || 'name'] group by option[to.groupProp || 'group'] for option in to.options`;
+        return {
+          ngModelAttrs: {
+            [ngOptions]: {
+              value: 'ng-options'
             }
-          };
-        }
+          }
+        };
       },
       apiCheck: {
         templateOptions: c.shape({
